27ba61c6746ed58ae13d7c5d2d0e16218e1aded7
Caroline Warner <cewarner@email.neit.edu>
2024-10-13 16:46:12 -0400

    lab 2 functionality

--- w2/lab/functions.php
+++ w2/lab/functions.php
@@ -1,5 +1,41 @@
 <?php
 
-    function bmiCalc(){
-        
+    function metersCalc($heightFeet, $heightInches){
+
+        $heightMeters = ((float)$heightInches + ((float)$heightFeet * 12)) * 0.0254;
+        return $heightMeters;
+    }
+
+    function kgCalc($weight){
+        $weightKg = (float)$weight / 2.20462;
+        echo $weightKg;
+        return $weightKg;
+    }
+    function bmiCalc($heightMeters, $weightKg){
+        $bmi = $weightKg / ($heightMeters ** 2);
+        return $bmi;
+    }
+
+    function age($birthday) {
+        $birthDate = new DateTime($birthday);
+        $today = new DateTime();
+        $interval = $today -> diff($birthDate);
+        return $interval->y;
+    }
+    
+    function bmiClass($bmi) {
+        if($bmi < 18.5){
+            $classification = 'underweight';
+        }
+        else if($bmi > 18.5 AND $bmi < 24.9){
+            $classification = 'normal weight';
+        }
+        else if($bmi > 25 AND $bmi < 29.9){
+            $classification = 'overweight';
+        }
+        else {
+            $classification = 'obese';
+        }
+
+        return $classification;
     }
--- w2/lab/index.php
+++ w2/lab/index.php
@@ -1,15 +1,20 @@
 <?php
     include 'functions.php';
 
-    var_dump($_POST);
-
     $firstName = '';
     $lastName = '';
+
     $maritalStatus = '';
+
     $birthday = '';
+    $age = '';
+
     $heightFeet = '';
     $heightInches = '';
+
     $weight = '';
+
+    $error = '';
 
     if(filter_input(INPUT_POST, 'first_name') != ''){
         $firstName = filter_input(INPUT_POST, 'first_name');
@@ -25,9 +30,57 @@
         $error .= 'Last name must be filled in <br/>';
     }
 
-    //marital stat here//
+    if(filter_input(INPUT_POST, 'marital_status') != ''){
+        $maritalStatus = filter_input(INPUT_POST, 'marital_status');
+    }
+    else{
+        $error .= 'Marital status must be selected <br/>';
+    }
 
+    if (filter_input(INPUT_POST, 'birthday') && 
+    (new DateTime(filter_input(INPUT_POST, 'birthday')) > new DateTime('1900-01-01')) && 
+    (new DateTime(filter_input(INPUT_POST, 'birthday')) < new DateTime('tomorrow'))) {
+
+        $birthday = filter_input(INPUT_POST, 'birthday');
+    } 
+    else {
+        
+        $error .= 'Date of birth must be valid <br/>';
+    }
+
+    if(filter_input(INPUT_POST, 'height_feet') > 0 AND filter_input(INPUT_POST, 'height_feet') <= 10 ){
+        $heightFeet = filter_input(INPUT_POST, 'height_feet');
+    }
+    else{
+        $error .= 'Height must be valid <br/>';
+    }
+
+    if(filter_input(INPUT_POST, 'height_inches') > 0 AND filter_input(INPUT_POST, 'height_inches') <= 11 ){
+        $heightInches = filter_input(INPUT_POST, 'height_inches');
+    }
+    else{
+        $error .= 'Height must be valid <br/>';
+    }
+
+    if(filter_input(INPUT_POST, 'weight') >= 0.1 AND filter_input(INPUT_POST, 'weight') <= 1000 ){
+        $weight = filter_input(INPUT_POST, 'weight');
+    }
+    else{
+        $error .= 'Weight must be valid <br/>';
+    }
+
+    if ($error == ''){
+        $meters = metersCalc($heightFeet, $heightInches);
+
+        $weightKg = kgCalc($weight);
+
+        if ($meters > 0 AND $weightKg > 0){
+            $bmi = bmiCalc($meters, $weightKg);
     
+            $bmiClass = bmiClass($bmi);
+        }
+
+    }
 
 ?>
 
@@ -38,7 +91,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Patient Intake</title>
 </head>
-<body>
+<>
     <h1>Patient Intake</h1>
     <div class="form-wrapper">
         <form name="patient-intake-form" method="post">
@@ -55,14 +108,14 @@
             <div class="form-control">
                 <p>Marital Status: </p>
                 <label for="marital_status">Married </label>
-                <input type="radio" id="married" name="marital_status" value="<?= $maritalStatus; ?>">
+                <input type="radio" id="married" name="marital_status" value="Married" <?= ($maritalStatus == 'Married') ? 'checked' : ''; ?>>
                 <label for="marital_status">Not Married </label>
-                <input type="radio" id="not_married" name="marital_status" value="<?= $maritalStatus; ?>"></div><br/>
-            </div>
+                <input type="radio" id="not_married" name="marital_status" value="Not Married" <?= ($maritalStatus == 'Not Married') ? 'checked' : ''; ?>>
+            </div><br/>
 
             <div class="form-control">
                 <label for="birthday">Date of Birth: </label><br/>
-                <input type="date" id="birthday" name="birthday" value="<?= $birthday; ?>">
+                <input type="date" id="birthday" name="birthday" min="" value="<?= htmlspecialchars($birthday); ?>">
             </div>
 
             <div class="form-control">
@@ -78,12 +131,32 @@
 
             <div class="form-control">
                 <label for="weight">Weight: </label><br/>
-                <input type="number" id="weight" name="weight" min="0.1" step="0.1" value="<?= $weight; ?>">
+                <input type="number" id="weight" name="weight" min="0.1" max="1000" step="0.1" value="<?= $weight; ?>">
+                <br />
+                <br />
+            </div>
+            
+            <div class="form-control">
+            <input type="submit" id="submit" name="submit" value="Submit">
             </div>
 
         </form>
 
     </div>
+
+
+    <p style="color: red;"><?= $error; ?></p>
+
+    <?php if($error == '' AND $_SERVER["REQUEST_METHOD"] == "POST"): ?>
+        <p>first name: </p><?= $firstName; ?>
+        <p>last name: </p><?= $lastName; ?>
+        <p>marital status: </p><?= $maritalStatus; ?>
+        <p>date of birth: </p><?= $birthday; ?>
+        <p>age: </p><?= age($birthday); ?>
+        <p>height: </p><?= $heightFeet . "'" . " " . $heightInches . '"'; ?>
+        <p>weight: </p><?= $weight . " lbs"; ?>
+        <p>bmi: </p><?= round($bmi, 2) . " - " . $bmiClass; ?>
+    <?php endif; ?>
 
 </body>
 </html>
